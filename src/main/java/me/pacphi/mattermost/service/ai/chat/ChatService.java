c8d272c3e5395f7e1746998ace6935f2aa3ffd4d
Chris Phillipson <cphillipson@pivotal.io>
2025-01-14 13:34:20 -0800

    Revert coercion in ChatServie. The prior implementation is more fluid in response.

--- src/main/java/me/pacphi/mattermost/service/ai/chat/ChatService.java
+++ src/main/java/me/pacphi/mattermost/service/ai/chat/ChatService.java
@@ -1,13 +1,10 @@
 package me.pacphi.mattermost.service.ai.chat;
 
-import me.pacphi.mattermost.service.ai.domain.PostLite;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -21,38 +18,34 @@ public class ChatService {
 
     public ChatService(ChatModel model, VectorStore vectorStore) {
         this.chatClient = ChatClient.builder(model)
-        		.defaultAdvisors(
-        				new SimpleLoggerAdvisor())
-        		.build();
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor())
+                .build();
         this.vectorStore = vectorStore;
     }
 
     public String respondToQuestion(String question) {
-        return respondToQuestion(question, null);
+        return constructRequest(question, null)
+                .call()
+                .content();
     }
 
     public String respondToQuestion(String question, List<FilterMetadata> filterMetadata) {
-        List<PostLite> posts = constructRequest(question, filterMetadata)
+        return constructRequest(question, filterMetadata)
                 .call()
-                .entity(new ParameterizedTypeReference<List<PostLite>>() {});
-        return constructResponse(posts);
+                .content();
     }
 
     public Flux<String> streamResponseToQuestion(String question) {
-        return streamResponseToQuestion(question, null);
+        return constructRequest(question, null)
+                .stream()
+                .content();
     }
 
     public Flux<String> streamResponseToQuestion(String question, List<FilterMetadata> filterMetadata) {
-        List<PostLite> posts = constructRequest(question, filterMetadata)
-                .call()
-                .entity(new ParameterizedTypeReference<List<PostLite>>() {});
-        if (CollectionUtils.isEmpty(posts)) {
-            return Flux.empty();
-        } else {
-            return Flux
-                    .fromIterable(posts)
-                    .map(post -> String.join(System.lineSeparator(), post.asResponse(), System.lineSeparator(), System.lineSeparator()));
-        }
+        return constructRequest(question, filterMetadata)
+                .stream()
+                .content();
     }
 
     private ChatClient.ChatClientRequestSpec constructRequest(String question, List<FilterMetadata> filterMetadata) {
@@ -65,16 +58,5 @@ public class ChatService {
                         )
                         .build())
                 .user(question);
-    }
-
-    private String constructResponse(List<PostLite> posts) {
-        StringBuilder sb = new StringBuilder();
-        if (CollectionUtils.isNotEmpty(posts)) {
-            for (PostLite post : posts) {
-                sb.append(post.asResponse());
-            }
-            sb.append(System.lineSeparator());
-        }
-        return sb.toString();
     }
 }
